fb224ce31a008f34ddda3ac81b752c52d7a32cc9
SeokHyeon <j483679@gmail.com>
2019-05-27 03:12:23 +0900

    no.4 commit plus View,Controller

--- app.js
+++ app.js
@@ -3,20 +3,14 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import {
-    userRouter
-} from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello From Home");
-
-const handleProfile = (req, res) => res.send("you are on my Profile");
-
-const betweenHome = (req, res, next) => {
-    console.log("Between");
-    next();
-};
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -25,10 +19,8 @@ app.use(bodyParser.json());
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
---
+++ controllers/userController.js
@@ -0,0 +1,7 @@
+export const join = (req, res) => res.render("join");
+export const login = (req, res) => res.render("login");
+export const logout = (req, res) => res.render("logout");
+export const users = (req, res) => res.render("users");
+export const user_detail = (req, res) => res.render("user_detail");
+export const edit_profile = (req, res) => res.render("edit_profile");
+export const change_password = (req, res) => res.render("change_password");
